587d1c834a33218936f492bb3ba151923c34d89c
BlackSploit <blacksploitx0@gmail.com>
2025-06-16 21:13:26 +0530

    v6

--- index.js
+++ index.js
@@ -12,3 +12,29 @@
 
 
 require("dotenv").config();
+
+console.log("DB CONNECTED");
+const { userRouter} = require("./routes/user");
+const {courseRouter}= require("./routes/course");
+const {adminRouter}= require("./routes/admin")
+
+const express = require("express");
+const app= express();
+const mongoose = require("mongoose");
+
+
+app.use(express.json());
+
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/admin", adminRoutes);
+app.use("/api/v1/course", courseRoutes);
+
+
+async fuction main(){
+    await mongoose.connect(process.env.MONGO_URL);
+
+    app.listen(3000);
+    console.log("BE RUNNING DB CONNECTED");
+}
+
+main();
